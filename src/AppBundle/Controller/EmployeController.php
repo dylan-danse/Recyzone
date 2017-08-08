32d76614a8174516a9da95727726f0a0f9801717
dylandanse <dylan.danse@c-services.lu>
2017-08-08 18:10:53 +0200

    statistique query working

--- src/AppBundle/Controller/EmployeController.php
+++ src/AppBundle/Controller/EmployeController.php
@@ -27,57 +27,18 @@ class EmployeController extends Controller
      */
     public function indexAction(Request $request)
     {
-        /* TODO : TO BE REACTIVATED, DESACTIVATED FOR TESTING PURPOSE
-        $currentTime = date('H:i a');
-        $openingWeekMorning =         \DateTime::createFromFormat('H:i', "09:00");
-        $closingWeekMorning =         \DateTime::createFromFormat('H:i', "12:15");
-        $openingWeekAfternoon =       \DateTime::createFromFormat('H:i', "13:00");
-        $closingWeekAfternoon =       \DateTime::createFromFormat('H:i', "15:45");
-        $openingSaturdayMorning =     \DateTime::createFromFormat('H:i', "08:30");
-        $closingSaturdayMorning =     \DateTime::createFromFormat('H:i', "12:15");
-        $openingSaturdayAfternoon =   \DateTime::createFromFormat('H:i', "13:00");
-        $closingSaturdayAfternon =    \DateTime::createFromFormat('H:i', "16:45");
+        $this->redirectIfNotEmploye();
 
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery("select n from AppBundle\Entity\Notification n inner join n.park as p where p.id = :parkID and n.isDeleted=0");
+        $query->setParameters(array(
+            'parkID' => $this->getUser()->getPark()->getId()
+        ));
+        $notifications = $query->getResult();
 
-        switch (date('D')){
-            case 'Sun':
-            case 'Mon':
-                return $this->render('employe/depositClosed.html.twig');
-            case 'Tue':
-            case 'Wed':
-            case 'Thu':
-            case 'Fri':
-                if ($currentTime < $openingWeekMorning && $currentTime > $closingWeekMorning ||
-                    $currentTime < $openingWeekAfternoon && $currentTime > $closingWeekAfternoon){
-                    return $this->render('employe/depositClosed.html.twig');
-                }
-                break;
-            case 'Sat':
-                if ($currentTime < $openingSaturdayMorning && $currentTime > $closingSaturdayMorning ||
-                    $currentTime < $openingSaturdayAfternoon && $currentTime > $closingSaturdayAfternon){
-                    return $this->render('employe/depositClosed.html.twig');
-                }
-                break;
-        }*/
-
-        if ($this->getUser() == null){
-            return $this->redirect('./login');
-        }
-        elseif ($this->getUser()->getRole()->getName() != 'Employé'){
-            return $this->redirect('./error');
-        }
-        else{
-            $em = $this->getDoctrine()->getManager();
-            $query = $em->createQuery("select n from AppBundle\Entity\Notification n inner join n.park as p where p.id = :parkID and n.isDeleted=0");
-            $query->setParameters(array(
-                'parkID' => $this->getUser()->getPark()->getId()
-            ));
-            $notifications = $query->getResult();
-
-            return $this->render('employe/index.html.twig',array(
-                'notifications' => $notifications
-            ));
-        }
+        return $this->render('employe/index.html.twig',array(
+            'notifications' => $notifications
+        ));
     }
 
     /**
@@ -85,59 +46,52 @@ class EmployeController extends Controller
      */
     public function addHousehold(Request $request)
     {
-        if ($this->getUser() == null){
-            return $this->redirect('./login');
+        $this->redirectIfNotEmploye();
+
+        $form = $this->createFormBuilder()
+            ->add('firstname', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('lastName', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('email', EmailType::class, array('attr' => array('class' => 'form-control')))
+            ->add('streetName', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('houseNumber', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('houseBox', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('commune', TextType::class, array('attr' => array('class' => 'form-control')))//choiceType
+            ->add('city', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('numberOfAdult', IntegerType::class, array('attr' => array('class' => 'form-control')))
+            ->add('numberOfChild', IntegerType::class, array('attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array('label' => 'Create Household', 'attr' => array('class' => 'btn btn-primary')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $firstname = $form['firstname']->getData();
+            $lastName = $form['lastName']->getData();
+            $email = $form['email']->getData();
+            $streetName = $form['streetName']->getData();
+            $houseNumber = $form['houseNumber']->getData();
+            $houseBox = $form['houseBox']->getData();
+            $commune = $form['commune']->getData();
+            $city = $form['city']->getData();
+            $numberOfAdult = $form['numberOfAdult']->getData();
+            $numberOfChild = $form['numberOfChild']->getData();
+
+            $userManager = $this->container->get('fos_user.user_manager');
+            $userAdmin = new User('random',$email,explode("@", $email, 2)[0],
+                $firstname,$lastName,$streetName,$houseNumber,$houseBox,$commune,$city,$numberOfChild,$numberOfAdult,
+                $this->getDoctrine()->getRepository("AppBundle:Role")->findOneByName(array("Ménage")),
+                $this->getUser()->getPark());
+            // TODO : replace with random password generation
+
+            $userManager->updateUser($userAdmin);
+
+            return $this->redirectToRoute('employe');
         }
-        elseif ($this->getUser()->getRole()->getName() != 'Employé'){
-            return $this->redirect('./error');
-        }
-        else{
 
-            $form = $this->createFormBuilder()
-                ->add('firstname', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('lastName', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('email', EmailType::class, array('attr' => array('class' => 'form-control')))
-                ->add('streetName', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('houseNumber', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('houseBox', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('commune', TextType::class, array('attr' => array('class' => 'form-control')))//choiceType
-                ->add('city', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('numberOfAdult', IntegerType::class, array('attr' => array('class' => 'form-control')))
-                ->add('numberOfChild', IntegerType::class, array('attr' => array('class' => 'form-control')))
-                ->add('save', SubmitType::class, array('label' => 'Create Household', 'attr' => array('class' => 'btn btn-primary')))
-                ->getForm();
-
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()){
-
-                $firstname = $form['firstname']->getData();
-                $lastName = $form['lastName']->getData();
-                $email = $form['email']->getData();
-                $streetName = $form['streetName']->getData();
-                $houseNumber = $form['houseNumber']->getData();
-                $houseBox = $form['houseBox']->getData();
-                $commune = $form['commune']->getData();
-                $city = $form['city']->getData();
-                $numberOfAdult = $form['numberOfAdult']->getData();
-                $numberOfChild = $form['numberOfChild']->getData();
-
-                $userManager = $this->container->get('fos_user.user_manager');
-                $userAdmin = new User('random',$email,explode("@", $email, 2)[0],
-                    $firstname,$lastName,$streetName,$houseNumber,$houseBox,$commune,$city,$numberOfChild,$numberOfAdult,
-                    $this->getDoctrine()->getRepository("AppBundle:Role")->findOneByName(array("Ménage")),
-                    $this->getUser()->getPark());
-                // TODO : replace with random password generation
-
-                $userManager->updateUser($userAdmin);
-
-                return $this->redirectToRoute('employe');
-            }
-
-            return $this->render('employe/addHousehold.html.twig', array(
-                'form' => $form->createView()
-            ));
-        }
+        return $this->render('employe/addHousehold.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
     /**
@@ -145,19 +99,16 @@ class EmployeController extends Controller
      */
     public function makeDeposit(Request $request)
     {
-        if ($this->getUser() == null){
-            return $this->redirect('./login');
-        }
-        elseif ($this->getUser()->getRole()->getName() != 'Employé'){
-            return $this->redirect('./error');
-        }
-        else{
-            $userManager = $this->container->get('fos_user.user_manager');
-            $users = $userManager->findUsers();
-            return $this->render('employe/makeDeposit.html.twig', array(
-                'users' => $users
-            ));
-        }
+        $this->redirectIfNotEmploye();
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+        $em = $this->getDoctrine();
+        $wasteTypes = $em->getRepository('AppBundle:WasteType')->findAll();
+        return $this->render('employe/makeDeposit.html.twig', array(
+            'users' => $users,
+            'waste_types' => $wasteTypes
+        ));
     }
 
     /**
@@ -187,76 +138,82 @@ class EmployeController extends Controller
      */
     public function addWastes(Request $request)
     {
-        try{
-            $temp = json_decode($request->getContent(), true);
-
-            $em = $this->getDoctrine()->getManager();
-            $query = $em->createQueryBuilder();
-
-            foreach($temp as $item){
-                $houseHold = $em->getRepository("AppBundle:User")->find($item['user']);
-                $wasteType = $em->getRepository("AppBundle:WasteType")->findOneBy(array('name' => $item['type']));
-                $volume = substr($item['volume'],0,strlen($item['volume'])-2);
-                $query->select('c')
-                    ->from('AppBundle:Container', 'c')
-                    ->leftJoin('c.park', 'p')
-                    ->leftJoin('c.waste_type','w')
-                    ->where("w.name = ?2")
-                    ->andWhere('p.id = ?1')
-                    ->andWhere('c.capacity-c.usedVolume > ?3')
-                    ->orderBy('c.capacity-c.usedVolume', 'ASC')
-                    ->setParameters(
-                        array(
-                            1 => $this->getUser()->getPark()->getId(),
-                            2 => $wasteType->getName(),
-                            3 => $volume
-                        )
+        $temp = json_decode($request->getContent(), true);
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQueryBuilder();
+        foreach($temp as $item){
+            $houseHold = $em->getRepository("AppBundle:User")->find($item['user']);
+            $wasteType = $em->getRepository("AppBundle:WasteType")->findOneBy(array('name' => $item['type']));
+            $volume = substr($item['volume'],0,strlen($item['volume'])-2);
+            $query->select('c')
+                ->from('AppBundle:Container', 'c')
+                ->leftJoin('c.park', 'p')
+                ->leftJoin('c.waste_type','w')
+                ->where("w.name = ?2")
+                ->andWhere('p.id = ?1')
+                ->andWhere('c.capacity-c.usedVolume >= ?3')
+                ->orderBy('c.capacity-c.usedVolume', 'ASC')
+                ->setParameters(
+                    array(
+                        1 => $this->getUser()->getPark()->getId(),
+                        2 => $wasteType->getName(),
+                        3 => $volume
                     )
-                    ->setMaxResults(1);
+                )
+                ->setMaxResults(1);
+            try{
                 $container = $query->getQuery()->getSingleResult();
-                $em->persist(new Deposit($volume,0,new \DateTime("now"),$wasteType,$houseHold,$container));
-                $container->setUsedVolume($container->getUsedVolume()+$volume);
-                if($container->getCompletionPercentage() >= 80){
-                    $em->persist(new Notification(new \DateTime("now"),"Conteneur".$container->getId()." rempli à ".$container->getCompletionPercentage()."%", false,$this->getUser()->getPark()));
-                }
-                $em->flush();
+            } catch (\Exception $ex){
+                return new Response("Dépôt impossible pour ".$volume."m3 (".$item['type'].") ! Aucun conteneur ne possède la capacité suffisante", 500);
             }
-            return new JsonResponse("Dépôt effectué avec succès !", 200);
+            $em->persist(new Deposit($volume,0,new \DateTime("now"),$wasteType,$houseHold,$container));
+            $container->setUsedVolume($container->getUsedVolume()+$volume);
+            if($container->getCompletionPercentage() >= 80){
+                $em->persist(new Notification(new \DateTime("now"),"Conteneur".$container->getId()." rempli à ".$container->getCompletionPercentage()."%", false,$this->getUser()->getPark()));
+            }
+            $em->flush();
+        }
+        return new JsonResponse("Dépôts effectué avec succès !", 200);
+    }
 
-        } catch (Exception $ex){
-            return new JsonResponse($ex->getMessage(), 500);
+    public function redirectIfNotEmploye(){
+        if ($this->getUser() == null){
+            return $this->redirect('./login');
+        }
+        elseif ($this->getUser()->getRole()->getName() != 'Employé'){
+            return $this->redirect('./error');
         }
     }
 
-    /**
-     * @Route("/test", name="test")
-     */
-    public function test(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQueryBuilder();
-        $houseHold = $em->getRepository("AppBundle:User")->find(1);
-        $wasteType = $em->getRepository("AppBundle:WasteType")->find(1);
-
-        $query->select('c.id as id, c.capacity-c.usedVolume as remaining_volume')
-            ->from('AppBundle:Container', 'c')
-            ->leftJoin('c.park', 'p')
-            ->leftJoin('c.waste_type','w')
-            ->where("w.name = ?2")
-            ->andWhere('p.id = ?1')
-            ->andWhere('c.capacity-c.usedVolume > ?3')
-            ->orderBy('c.capacity-c.usedVolume', 'ASC')
-            ->setParameters(
-                array(
-                    1 => 1,
-                    2 => "déchets de jardin",
-                    3 => 30
-                )
-            )
-            ->setMaxResults(1);
-
-        $result = $query->getQuery()->getResult();
-        print_r($result);
-        return new JsonResponse("", 200);
+    public function redirectIfParkClosed(){
+        $currentTime = date('H:i a');
+        $openingWeekMorning =         \DateTime::createFromFormat('H:i', "09:00");
+        $closingWeekMorning =         \DateTime::createFromFormat('H:i', "12:15");
+        $openingWeekAfternoon =       \DateTime::createFromFormat('H:i', "13:00");
+        $closingWeekAfternoon =       \DateTime::createFromFormat('H:i', "15:45");
+        $openingSaturdayMorning =     \DateTime::createFromFormat('H:i', "08:30");
+        $closingSaturdayMorning =     \DateTime::createFromFormat('H:i', "12:15");
+        $openingSaturdayAfternoon =   \DateTime::createFromFormat('H:i', "13:00");
+        $closingSaturdayAfternon =    \DateTime::createFromFormat('H:i', "16:45");
+        switch (date('D')){
+            case 'Sun':
+            case 'Mon':
+                return $this->render('employe/depositClosed.html.twig');
+            case 'Tue':
+            case 'Wed':
+            case 'Thu':
+            case 'Fri':
+                if ($currentTime < $openingWeekMorning && $currentTime > $closingWeekMorning ||
+                    $currentTime < $openingWeekAfternoon && $currentTime > $closingWeekAfternoon){
+                    return $this->render('employe/depositClosed.html.twig');
+                }
+                break;
+            case 'Sat':
+                if ($currentTime < $openingSaturdayMorning && $currentTime > $closingSaturdayMorning ||
+                    $currentTime < $openingSaturdayAfternoon && $currentTime > $closingSaturdayAfternon){
+                    return $this->render('employe/depositClosed.html.twig');
+                }
+                break;
+        }
     }
 }
--- src/AppBundle/Controller/GerantController.php
+++ src/AppBundle/Controller/GerantController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class GerantController extends Controller
@@ -16,24 +17,18 @@ class GerantController extends Controller
      */
     public function indexAction(Request $request)
     {
-        if ($this->getUser() == null){
-            return $this->redirect('./login');
-        }
-        elseif ($this->getUser()->getRole()->getName() != 'Gérant'){
-            return $this->redirect('./error');
-        }
-        else{
-            $em = $this->getDoctrine()->getManager();
-            $query = $em->createQuery("select n from AppBundle\Entity\Notification n inner join n.park as p where p.id = :parkID and n.isDeleted=0");
-            $query->setParameters(array(
-                'parkID' => $this->getUser()->getPark()->getId()
-            ));
-            $notifications = $query->getResult();
+        $this->redirectIfNotGerant();
 
-            return $this->render('gerant/index.html.twig',array(
-                'notifications' => $notifications
-            ));
-        }
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery("select n from AppBundle\Entity\Notification n inner join n.park as p where p.id = :parkID and n.isDeleted=0");
+        $query->setParameters(array(
+            'parkID' => $this->getUser()->getPark()->getId()
+        ));
+        $notifications = $query->getResult();
+
+        return $this->render('gerant/index.html.twig',array(
+            'notifications' => $notifications
+        ));
     }
 
     /**
@@ -64,25 +59,19 @@ class GerantController extends Controller
      */
     public function occupations(Request $request)
     {
-        if ($this->getUser() == null){
-            return $this->redirect('./login');
-        }
-        elseif ($this->getUser()->getRole()->getName() != 'Gérant'){
-            return $this->redirect('./error');
-        }
-        else{
-            $em = $this->getDoctrine()->getManager();
-            $query = $em->createQuery(
-                "select n from AppBundle\Entity\Container n left join n.park as p left join n.waste_type as w where p.id = :parkID order by w.name ASC"
-            );
-            $query->setParameters(array(
-                'parkID' => $this->getUser()->getPark()->getId()
-            ));
-            $containers = $query->getResult();
-            return $this->render('gerant/occupations.html.twig',array(
-                'containers' => $containers
-            ));
-        }
+        $this->redirectIfNotGerant();
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            "select n from AppBundle\Entity\Container n left join n.park as p left join n.waste_type as w where p.id = :parkID order by w.name ASC"
+        );
+        $query->setParameters(array(
+            'parkID' => $this->getUser()->getPark()->getId()
+        ));
+        $containers = $query->getResult();
+        return $this->render('gerant/occupations.html.twig',array(
+            'containers' => $containers
+        ));
     }
 
     /**
@@ -90,24 +79,67 @@ class GerantController extends Controller
      */
     public function statistiques(Request $request)
     {
+        $this->redirectIfNotGerant();
+
+        $em = $this->getDoctrine();
+        $wasteTypes = $em->getRepository('AppBundle:WasteType')->findAll();
+
+        return $this->render('gerant/statistiques.html.twig',array(
+            'waste_types' => $wasteTypes
+        ));
+    }
+
+    /**
+     * @Route("/statistiques/{period}/{type}/{waste_id}", name="statistiques2")
+     */
+    public function getStatistiques($period,$type,$waste_id){
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQueryBuilder();
+        $parkId = $this->getUser()->getPark()->getId();
+        
+        $periodDQL = "";
+        $volumeOrVisiteDQL = "";
+        $wasteDQL = "";
+
+        if($period == "journalier"){
+            $periodDQL = "DAYNAME";
+        }
+        else if($period == "mensuel") {
+            $periodDQL = "MONTHNAME";
+        }
+
+        if($type == "volume"){
+            $volumeOrVisiteDQL = "SUM";
+            if($waste_id){
+                $wasteDQL = $waste_id;
+            }
+        }
+        else if($type == "visite"){
+            $volumeOrVisiteDQL = "COUNT";
+        }
+
+        $query->select($periodDQL.'(d.creationDate) as key, '.$volumeOrVisiteDQL.'(d.quantity) as value')
+            ->from('AppBundle:Deposit', 'd')
+            ->leftJoin('d.container','c')
+            ->leftJoin('c.park','p')
+            ->leftJoin('d.waste_type','w')
+            ->where('p.id = '.$parkId)
+            ->andWhere(($wasteDQL === "")?'true':'w.id='.$wasteDQL)
+            ->groupBy('key');
+
+        $stats = $query->getQuery()->getResult();
+
+        print_r($stats);
+        return new Response();
+    }
+
+    public function redirectIfNotGerant(){
         if ($this->getUser() == null){
             return $this->redirect('./login');
         }
         elseif ($this->getUser()->getRole()->getName() != 'Gérant'){
             return $this->redirect('./error');
-        }
-        else{
-            /*$em = $this->getDoctrine()->getManager();
-            $query = $em->createQuery(
-                "select n from AppBundle\Entity\Container n left join n.park as p left join n.waste_type as w where p.id = :parkID order by w.name ASC"
-            );
-            $query->setParameters(array(
-                'parkID' => $this->getUser()->getPark()->getId()
-            ));
-            $containers = $query->getResult();*/
-            return $this->render('gerant/statistiques.html.twig',array(
-
-            ));
         }
     }
 }
